e1668de5a838de8b2561c3a328d3a0fb9dddba9f
i4vk <ivangs98@gmail.com>
2019-10-01 18:53:27 +0200

    Creada clase GymManager y modificado el .gitignore para ignorar archivos .json. Avanza en #5

---
+++ src/gymManager.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os.path as path
+
+class Database:
+    db = None
+    num_clientes = None
+
+    def __init__(self):
+        if path.isfile("database.json"):
+            f = open("database.json")
+            self.db = json.load(f)
+        else:
+            json_db = '''{
+                          "clientes": [
+                            {
+                        	    "nombre": "Iván",
+                        		"apellidos": "Garzón Segura",
+                        		"dni": "1234567S",
+                        		"email": "ivangarzon98@correo.ugr.es",
+                                "id": 1
+                            }
+                          ]
+                        }'''
+            #Crea database con una entrada
+            self.db = json.loads(json_db)
+            f = open("database.json", 'w')
+            json.dump(self.db, f, indent=2)
+
+        self.num_clientes = len(self.db["clientes"])
+
+    def save(self, path):
+        f = open(path, 'w')
+        json.dump(self.db, f, indent=2)
+
+    def insertarCliente(self, cliente):
+        cliente["id"] = self.num_clientes + 1
+        self.db["clientes"].append(cliente)
+
+    def eliminarCliente(self, id):
+        for i in range(len(self.db["clientes"])):
+            if self.db["clientes"][i]["id"] == 2:
+                self.db["clientes"].pop(i)
+
+
+
+if __name__ == "__main__":
+    clientes = Database()
+
+    un_cliente = {"nombre": "Antonio", "apellidos": "García García", "dni":"1234", "email": "papaya"}
+    clientes.insertarCliente(un_cliente)
+    print(clientes.db, "\n")
+    clientes.eliminarCliente(2)
+    print(clientes.db)
