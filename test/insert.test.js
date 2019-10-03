d93e312e21ff983dda532bc863d3cfd95de8951e
i4vk <ivangs98@gmail.com>
2019-10-03 12:40:33 +0200

    Fixed insert.test.js. #6

--- test/insert.test.js
+++ test/insert.test.js
@@ -11,11 +11,12 @@ beforeAll(() => {
   clientes.load('./src/database.json');
 });
 
-test('inserta correctamente', () => {
+test('Inserta correctamente', () => {
+  var ini_num_clientes = clientes.num_clientes;
   var nuevo_cliente = {nombre:"Antonio",apellidos:"Papaya Telescopio",dni:"1234567S",email:"ivangarzon98@correo.ugr.es"};
   clientes.insert(nuevo_cliente);
 
   var cliente = clientes.getCliente(clientes.num_clientes);
   expect(cliente).toEqual(nuevo_cliente);
-  expect(clientes.num_clientes).toBe(2);
+  expect(clientes.num_clientes).toBe(ini_num_clientes+1);
 });
