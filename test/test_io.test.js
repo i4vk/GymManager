67378e0f69e14d31b7593189a5f0efaf5aa7dbd7
i4vk <ivangs98@gmail.com>
2019-10-05 18:47:21 +0200

    Implementado test save database. #6

--- test/test_io.test.js
+++ test/test_io.test.js
@@ -2,6 +2,10 @@ var GymManager = require('../src/gymManager.js');
 
 var clientes = new GymManager('./test/data/database.json');
 
+beforeEach(() => {
+  clientes.clear();
+});
+
 test('Carga archivo correctamente', () => {
   clientes.load("./test/data/prueba_io.json");
   db_esperada = {
@@ -20,6 +24,22 @@ test('Carga archivo correctamente', () => {
       }
     }
   };
+
+  expect(clientes.db).toEqual(db_esperada);
+});
+
+test('Guarda correctamente', async () => {
+  var un_cliente = {nombre:"Iván",apellidos:"Garzón Segura",dni:"1234567S",email:"ivangarzon98@correo.ugr.es"};
+  var un_cliente2 = {nombre:"Antonio",apellidos:"Papaya Telescopio",dni:"1234567S",email:"papaya@correo.ugr.es"};
+
+  clientes.insert(un_cliente);
+  clientes.insert(un_cliente2);
+
+  await clientes.save("./test/data/temp.json");
+  db_esperada = clientes.db;
+
+  clientes.clear();
+  clientes.load("./test/data/temp.json");
 
   expect(clientes.db).toEqual(db_esperada);
 });
