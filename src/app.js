0139093f13056cbea2d535c992f23204297d438d
i4vk <ivangs98@gmail.com>
2019-10-22 21:10:45 +0200

    Modificado error handler de ruta no especificada

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var error = new Error("No existe la ruta especificada");
+  error.status = 404;
+  next(error);
 });
 
 // error handler
