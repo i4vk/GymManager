278a20bba06194291b6b321bdbf5cc2aff5d0ca8
i4vk <ivangs98@gmail.com>
2019-10-28 13:23:41 +0100

    Añadidos test put y delete de un cliente. #15

--- test/api.test.js
+++ test/api.test.js
@@ -33,21 +33,42 @@ describe('GET /clientes/name/', function() {
   test('Obtiene cliente por nombre', async () => {
     var nombre_cliente = {"nombre": "Iván", "apellidos": "Garzón Segura"};
     return request.get('/clientes/name/').send(nombre_cliente).then(response => {
-      cliente_esperado = {
-                          "nombre": "Iván",
-                          "apellidos": "Garzón Segura",
-                          "dni": "1234567S",
-                          "email": "ivangarzon98@correo.ugr.es"
-                        };
       expect(response.statusCode).toBe(200);
       expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
     })
   });
 
   test('No encuentra cliente falso', async () => {
-    var nombre_cliente = {"nombre": "Federico", "apellidos": "García García"};
+    var nombre_cliente = {"nombre": "Cliente", "apellidos": "Falso"};
     return request.get('/clientes/name/').send(nombre_cliente).then(response => {
       expect(response.statusCode).toBe(404);
     })
-  })
+  });
+})
+
+describe('POST /clientes', function() {
+  test('Inserta correctamente un cliente', async () => {
+    return request.post('/clientes').send(nuevo_cliente).then(response => {
+      expect(response.statusCode).toBe(201);
+      expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    })
+  });
+})
+
+describe('PUT /clientes/id', function() {
+  test('Modifica un cliente correctamente', async () => {
+    campos_modificados = {"dni":"9999999V", "email":"modificado@hello.com"};
+    return request.put('/clientes/id/2').send(campos_modificados).then(response => {
+      expect(response.statusCode).toBe(201);
+      expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    })
+  });
+})
+
+describe('DELETE /clientes/id', function() {
+  test('Elimina cliente correctamente', async () => {
+    return request.delete('/clientes/id/1').then(response => {
+      expect(response.statusCode).toBe(200);
+    })
+  });
 })
