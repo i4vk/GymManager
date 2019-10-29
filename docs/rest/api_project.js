dd94adfb186a2fc789b0edb08beedf58a0449084
i4vk <ivangs98@gmail.com>
2019-10-29 22:59:26 +0100

    Añadida documentación api y clase principal

--- docs/rest/api_project.js
+++ docs/rest/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-10-29T19:26:02.671Z",
+    "time": "2019-10-29T21:57:22.994Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
