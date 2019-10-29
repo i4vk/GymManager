e18a592a94138dd372ffec7a6e380a2e67e7e292
i4vk <ivangs98@gmail.com>
2019-10-29 23:03:22 +0100

    Añadida task para generar documentación. Fix #12

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const pm2 = require('pm2');
 var exec = require('child_process').exec;
 var jest = require('gulp-jest').default;
+var apidoc = require('gulp-apidoc');
+var jsdoc = require('gulp-jsdoc3');
 
 
 gulp.task('install', function (cb) {
@@ -52,3 +54,16 @@ gulp.task('restart', function(cb) {
 gulp.task('test', () => (
     gulp.src('test', {read: false}).pipe(jest({reporter:'default'}))
 ));
+
+gulp.task('doc', function(done){
+  exec('jsdoc ./src/gymManager.js -d ./docs/gymManager', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    done(err);
+  });
+  apidoc({
+    src: "./src/routes",
+    dest: "./docs/rest",
+    config: "./"
+  }, done);
+})
