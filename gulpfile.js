6d3265b424073773ff755f5e83fca9edd5b60c38
i4vk <ivangs98@gmail.com>
2019-10-30 10:25:17 +0100

    Modificada tarea restart que se quedaba bloqueada. #12

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 var jest = require('gulp-jest').default;
 var apidoc = require('gulp-apidoc');
 
-
+// Tarea para instalar todas las dependencias del proyecto
 gulp.task('install', function (cb) {
   exec('npm install', function(err, stdout, stderr) {
     console.log(stdout);
@@ -13,6 +13,7 @@ gulp.task('install', function (cb) {
   });
 });
 
+// Tarea para iniciar el servidor creando una instancia de pm2
 gulp.task('start', function(cb) {
   pm2.connect(false, function(err) {
     if (err) {
@@ -34,6 +35,7 @@ gulp.task('start', function(cb) {
   });
 });
 
+// Detiene la instancia de pm2 del servidor en ejecución
 gulp.task('stop', function(cb) {
   exec('pm2 stop ./src/bin/www', function(err, stdout, stderr) {
     console.log(stdout);
@@ -42,18 +44,21 @@ gulp.task('stop', function(cb) {
   });
 });
 
+// Reinicia el servidor
 gulp.task('restart', function(cb) {
-  pm2.restart('./src/bin/www', function(err) {
-    if (err) {
-      throw err;
-    }
-  })
+  exec('pm2 restart ./src/bin/www', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 })
 
+// Ejecuta los test
 gulp.task('test', () => (
     gulp.src('test', {read: false}).pipe(jest({reporter:'default'}))
 ));
 
+// Comprueba la cobertura de los test sobre el código
 gulp.task('coveralls', function(cb) {
   exec('npm run coveralls', function(err, stdout, stderr) {
     console.log(stdout);
@@ -62,6 +67,7 @@ gulp.task('coveralls', function(cb) {
   });
 })
 
+// Compila toda la documentación del proyecto y la almacena en el directorio docs/
 gulp.task('doc', function(done){
   exec('jsdoc ./src/gymManager.js -d ./docs/gymManager', function(err, stdout, stderr) {
     console.log(stdout);
@@ -75,4 +81,5 @@ gulp.task('doc', function(done){
   }, done);
 });
 
+// Tarea default que instala los requisitos y ejecuta los test posteriormente
 gulp.task('default', gulp.series('install', 'test'));
