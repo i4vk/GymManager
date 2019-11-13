6ff2f498561f423fdf417ec727de00defe17018d
i4vk <ivangs98@gmail.com>
2019-11-13 20:19:51 +0100

    Modificada tarea gulp para heroku. #21

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('start', function(cb) {
   pm2.disconnect();
 });
 
-gulp.task('heroku', function(cb) {
+gulp.task('heroku:deploy', function(cb) {
   exec('node ./src/bin/www', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
