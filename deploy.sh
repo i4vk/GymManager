5f85fece697026ba172ed79b6c309fd5665cb6db
i4vk <ivangs98@gmail.com>
2019-11-14 18:44:30 +0100

    Añadidos deployment script para azure. #23

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ fi
 
 # 4. Run gulp for build
 if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then  
-  eval $NPM_CMD install gulp 
-  ./node_modules/.bin/gulp start-simple
+  eval $NPM_CMD install gulp -g
+  gulp start-simple
 fi  
