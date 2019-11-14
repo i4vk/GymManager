766dfd0bf184922820aabae9fef174f657ab1f90
i4vk <ivangs98@gmail.com>
2019-11-14 18:41:42 +0100

    Añadidos deployment script para azure. #23

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,13 @@
 
 echo Handling node.js gulp deployment.  
 
-# 1. Select node version  
-selectNodeVersion  
-
 # 2. Install npm packages  
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then  
   eval $NPM_CMD install  
-  exitWithMessageOnError "npm failed"  
 fi  
 
 # 4. Run gulp for build
 if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then  
   eval $NPM_CMD install gulp 
-  exitWithMessageOnError "installing gulpfailed"  
   ./node_modules/.bin/gulp start-simple
-  exitWithMessageOnError "gulp failed"  
 fi  
-
-# 5. KuduSync to Target  
-"$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"  
-exitWithMessageOnError "Kudu Sync to Target failed" 
